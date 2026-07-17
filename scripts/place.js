d8165fbf40937aa58ea38174f8daff0e99d94ca8
LAQB2020 <luisquezadaangel@gmail.com>
2026-07-16 21:57:29 -0600

    javascrip function added

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,23 @@ document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent =
     `Last Modification: ${document.lastModified}`;
+
+const temperature = parseFloat(document.getElementById("temperature").textContent);
+const windSpeed = parseFloat(document.getElementById("wind").textContent);
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.getElementById("windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
